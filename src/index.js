3186d7c101970adde4fc42cfdae0ebd7cfdf53cf
Rituparna Singh <singhri085@gmail.com>
2026-04-18 17:47:37 +0530

    Updated the cors4

--- src/index.js
+++ src/index.js
@@ -33,8 +33,8 @@ app.get("/health", async (req, res) => {
 
 app.use(cors({
   origin: [
-    "https://localhost:5173",
-    "api-monitor-frontend-final.vercel.app" 
+    "http://localhost:5173",
+    "https://api-monitor-frontend-final.vercel.app" 
   ]
 }));
 
