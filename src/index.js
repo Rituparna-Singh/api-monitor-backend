c410fdba33122b2d7d7b13c9b4f0677d57be4f82
Rituparna Singh <singhri085@gmail.com>
2026-04-18 17:13:16 +0530

    Updated the cors

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.get("/health", async (req, res) => {
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+   
     "https://api-monitor-frontend-final.vercel.app/" // ← paste your vercel URL here
   ]
 }));
