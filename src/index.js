18a7fb296173a4e72f148a93bd72a5ecb52a71da
Rituparna Singh <singhri085@gmail.com>
2026-04-18 17:15:32 +0530

    Updated the cors1

--- src/index.js
+++ src/index.js
@@ -24,10 +24,17 @@ app.get("/health", async (req, res) => {
   }
 });
 
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://api-monitor-frontend-final.vercel.app/" // ← paste your vercel URL here
+//   ]
+// }));
+
 app.use(cors({
   origin: [
-   
-    "https://api-monitor-frontend-final.vercel.app/" // ← paste your vercel URL here
+    "http://localhost:5173",
+    "https://api-monitor-frontend-final.vercel.app" // ✅ no slash
   ]
 }));
 
